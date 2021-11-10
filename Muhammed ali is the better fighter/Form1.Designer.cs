2bbc8072b172d0017aff850097c14adb3e7d103f
ETHANCPIPKIN <lucidfridaynights@gmail.com>
2021-11-09 22:41:48 -0800

    cloned it to the personal repositrory and began the assignment

---
+++ Muhammed ali is the better fighter/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿
+namespace Muhammed_ali_is_the_better_fighter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Jokebutton = new System.Windows.Forms.Button();
+            this.jokeText = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Jokebutton
+            // 
+            this.Jokebutton.Location = new System.Drawing.Point(331, 288);
+            this.Jokebutton.Name = "Jokebutton";
+            this.Jokebutton.Size = new System.Drawing.Size(155, 30);
+            this.Jokebutton.TabIndex = 0;
+            this.Jokebutton.Text = "Make me laugh";
+            this.Jokebutton.UseVisualStyleBackColor = true;
+            this.Jokebutton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // jokeText
+            // 
+            this.jokeText.AutoSize = true;
+            this.jokeText.Location = new System.Drawing.Point(387, 351);
+            this.jokeText.Name = "jokeText";
+            this.jokeText.Size = new System.Drawing.Size(36, 13);
+            this.jokeText.TabIndex = 1;
+            this.jokeText.Text = "Funny";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.jokeText);
+            this.Controls.Add(this.Jokebutton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Jokebutton;
+        private System.Windows.Forms.Label jokeText;
+    }
+}
+
