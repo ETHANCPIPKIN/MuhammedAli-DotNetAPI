b20cd8146c5b05e807e3921cec15ddd59bbf30d8
ETHANCPIPKIN <lucidfridaynights@gmail.com>
2021-11-10 22:27:19 -0800

    Finished up the requirements. Added the button to spawn a joke, and added the dropdown list

--- Muhammed ali is the better fighter/Form1.Designer.cs
+++ Muhammed ali is the better fighter/Form1.Designer.cs
@@ -31,13 +31,14 @@ namespace Muhammed_ali_is_the_better_fighter
         {
             this.Jokebutton = new System.Windows.Forms.Button();
             this.jokeText = new System.Windows.Forms.Label();
+            this.Box = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // Jokebutton
             // 
             this.Jokebutton.Location = new System.Drawing.Point(331, 288);
             this.Jokebutton.Name = "Jokebutton";
-            this.Jokebutton.Size = new System.Drawing.Size(155, 30);
+            this.Jokebutton.Size = new System.Drawing.Size(121, 30);
             this.Jokebutton.TabIndex = 0;
             this.Jokebutton.Text = "Make me laugh";
             this.Jokebutton.UseVisualStyleBackColor = true;
@@ -46,21 +47,31 @@ namespace Muhammed_ali_is_the_better_fighter
             // jokeText
             // 
             this.jokeText.AutoSize = true;
-            this.jokeText.Location = new System.Drawing.Point(387, 351);
+            this.jokeText.Location = new System.Drawing.Point(12, 9);
             this.jokeText.Name = "jokeText";
             this.jokeText.Size = new System.Drawing.Size(36, 13);
             this.jokeText.TabIndex = 1;
             this.jokeText.Text = "Funny";
+            // 
+            // Box
+            // 
+            this.Box.FormattingEnabled = true;
+            this.Box.Location = new System.Drawing.Point(331, 325);
+            this.Box.Name = "Box";
+            this.Box.Size = new System.Drawing.Size(121, 21);
+            this.Box.TabIndex = 2;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Box);
             this.Controls.Add(this.jokeText);
             this.Controls.Add(this.Jokebutton);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Funny gen";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -70,6 +81,7 @@ namespace Muhammed_ali_is_the_better_fighter
 
         private System.Windows.Forms.Button Jokebutton;
         private System.Windows.Forms.Label jokeText;
+        private System.Windows.Forms.ComboBox Box;
     }
 }
 
--- Muhammed ali is the better fighter/Form1.cs
+++ Muhammed ali is the better fighter/Form1.cs
@@ -1,12 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using ChuckNorrisAPI; 
 
 namespace Muhammed_ali_is_the_better_fighter
 {
@@ -17,9 +12,22 @@ namespace Muhammed_ali_is_the_better_fighter
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private async void button1_Click(object sender, EventArgs e)
         {
+            ChuckNorrisClient client = new ChuckNorrisClient();
+            Joke joke = await ChuckNorrisClient.GetRandomJoke();
 
+
+            jokeText.Text = joke.JokeText;
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            IEnumerable<string> categories = await ChuckNorrisClient.GetCategories();
+            foreach (var B in categories)
+            {
+                Box.Items.Add(B); 
+            }
         }
     }
 }
